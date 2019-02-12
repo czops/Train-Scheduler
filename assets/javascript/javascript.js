f0723b13bb3c5d93dcacadbd3006e6ef3248768e
czops <37029225+czops@users.noreply.github.com>
2019-02-11 21:34:01 -0600

    made progress toward displaying the minutes til the next arrival

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,15 +3,15 @@
 
 
 
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyCLMkRPJMDqLrPSI89hB_n2sOz_SWGCEME",
     authDomain: "train-scheduler-33c5e.firebaseapp.com",
     databaseURL: "https://train-scheduler-33c5e.firebaseio.com",
     projectId: "train-scheduler-33c5e",
     storageBucket: "train-scheduler-33c5e.appspot.com",
     messagingSenderId: "1060283539159"
-  };
+};
 
 
 firebase.initializeApp(config);
@@ -24,6 +24,7 @@ $("#add-row").on("click", function (event) {
     // Don't refresh the page!
     event.preventDefault();
 
+    debugger;
     // YOUR TASK!!!
     // Code in the logic for storing and retrieving the most recent user.
     // Don't forget to provide initial data to your Firebase database.
@@ -31,30 +32,84 @@ $("#add-row").on("click", function (event) {
     TrainDestination = $("#InputDestination").val().trim();
     FirstTrainTime = $("#InputFirstTrainTime").val().trim();
     TrainFrequency = $("#InputFrequency").val().trim();
-    //do calculations
-    NextTrain = moment().startOf(FirstTrainTime).fromNow();
-    // NextTrain = moment(FirstTrainTime).startOf().fromNow();
-    console.log(NextTrain);
+    console.log(TrainName);
+    console.log(TrainDestination);
+    console.log(FirstTrainTime);
+    console.log(TrainFrequency);
 
-    //this should not be the set method- want to instead use the push method
+    debugger;
+    //
 
-    database.ref().push({
-        Name: TrainName,
-        Destination: TrainDestination,
-        FirstTime: FirstTrainTime,
-        Frequency: TrainFrequency,
-        DateAdded: firebase.database.ServerValue.TIMESTAMP
-        // NextTrain: 
-    });
+    //This doesn't do anything
+
 
     // Refresh the entries
     $("#TrainNameInput").val("");
     $("#InputDestination").val();
     // $("#FirstTrainTime").val("--:-- AM/PM");
     $("#TrainFrequency").val();
+    //
+
+
+
+
+
+    debugger;
+    //getting moment to figure out the next train time
+    // Assumptions
+    var tFrequency = moment(TrainFrequency, "minutes");
+    console.log(tFrequency._i);
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(FirstTrainTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency._i;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency._i - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    // add an if statement here to check if the first train time has passed or not. If it has, use what's here, if it hasn't, use moment to output the difference between now and the first train time
+
+    if (moment(FirstTrainTime, "HH:mm") > moment()) {
+        var NextTrain = moment(FirstTrainTime, "HH:mm").add(tMinutesTillTrain, "HH:mm");
+        tMinutesTillTrain = moment(moment(FirstTrainTime, "HH:mm").subtract(moment(),"minutes"));
+        console.log("ARRIVAL TIME: " + moment(NextTrain).format("HH:mm"));
+    } else {
+        var NextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(NextTrain).format("HH:mm"));
+    }
+
+
+
+
+    //this should not be the set method- want to instead use the push method
+    debugger;
+    database.ref().push({
+        Name: TrainName,
+        Destination: TrainDestination,
+        FirstTime: FirstTrainTime,
+        Frequency: TrainFrequency,
+        NextTrainTime: moment(NextTrain).format("hh:mm"),
+        MinutesAway: tMinutesTillTrain,
+        DateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
 
 });
 
+debugger;
 //when you're accessing the database... use this to make sure the data is being inputted correctly....
 //instead of using the "value" you use "child-added" which creates a new object
 database.ref().on("child_added", function (snapshot) {
@@ -64,7 +119,9 @@ database.ref().on("child_added", function (snapshot) {
     console.log(snapshot.val().Name);
     console.log(snapshot.val().Destination);
     console.log(snapshot.val().FirstTime);
+    console.log(snapshot.val().NextTrainTime);
     console.log(snapshot.val().Frequency);
+    console.log(snapshot.val().MinutesAway);
 
     // Change the HTML to reflect the new row added to the database
     //This should work for every child the same way... it can't just target the same IDs each time
@@ -77,7 +134,7 @@ database.ref().on("child_added", function (snapshot) {
 
 
 
-    $("#myTable").append("<tr>" + "<td>" + snapshot.val().Name + "</td>" + "<td>" + snapshot.val().Destination + "</td>" + "<td>" + snapshot.val().Frequency + "</td>" + "<td>" + snapshot.val().FirstTime + "</td>" + "</tr>");
+    $("#myTable").append("<tr>" + "<td>" + snapshot.val().Name + "</td>" + "<td>" + snapshot.val().Destination + "</td>" + "<td>" + snapshot.val().Frequency + "</td>" + "<td>" + snapshot.val().NextTrainTime + "</td>" + "<td>" + snapshot.val().MinutesAway + "</td>" + "</tr>");
 
 
     // Handle the errors
